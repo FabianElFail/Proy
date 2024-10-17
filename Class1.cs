8efaf30cb379cd50f251e225e0b40cf85023826d
JairGarcia29 <jamesjair29@gmail.com>
2024-10-16 22:30:18 -0500

    Primer guardado

---
+++ Class1.cs
@@ -0,0 +1,18 @@
+﻿namespace ClassLibrary
+{
+    public class sum
+    {
+        public void sum1 () 
+        { 
+          int num1,num2;
+          double resul;
+            Console.WriteLine("Ingresa el primer numero: ");
+            num1=int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingresa el segundo numero: ");
+            num2=int.Parse(Console.ReadLine());
+            resul = num1 + num2;
+            Console.WriteLine("La suma de los numeros es: " + resul);
+             
+        }
+    }
+}
